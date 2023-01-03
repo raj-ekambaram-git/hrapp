143cb2ee0fa0c74db4495bcc6f1693dcad1dac6a
Raj Ekambaram <75325809+raj-ekambaram-git@users.noreply.github.com>
2023-01-02 18:50:35 -0600

    COmmit.

--- pages/timesheet/add.jsx
+++ pages/timesheet/add.jsx
@@ -2,11 +2,16 @@ import {MODE_ADD, EMPTY_STRING} from "../../constants/accountConstants";
 import { useRouter } from "next/router";
 import React from "react";
 import TimesheetAddEdit from "../../components/timesheet/timesheetAddEdit";
-
+import { useDispatch } from "react-redux";
+import {setTSEntries} from '../../store/modules/Timesheet/actions';
 
 
 const AddNewTimesheet = (props) => {
   const router = useRouter();
+  const dispatch = useDispatch();
+
+  //Resetting the TS Entries
+  dispatch(setTSEntries([]));
 
   const requestData = {
     mode: MODE_ADD,
