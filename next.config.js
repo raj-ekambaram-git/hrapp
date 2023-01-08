b91a0a0d7c67ea87e0a166173ac86bdd89a3a2e0
Raj Ekambaram <75325809+raj-ekambaram-git@users.noreply.github.com>
2023-01-08 10:42:25 -0600

    Updated the confg files

--- next.config.js
+++ next.config.js
@@ -7,5 +7,17 @@ module.exports = {
       apiUrl: process.env.NODE_ENV === 'development'
           ? 'http://localhost:3000/api' // development api
           : 'http://localhost:3000/api' // production api
-  }
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 }
