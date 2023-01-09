c46b46991acb61da2308ef41b028860ddd5452e3
Raj Ekambaram <75325809+raj-ekambaram-git@users.noreply.github.com>
2023-01-09 15:06:02 -0600

    Auth handled

--- middleware.ts
+++ middleware.ts
@@ -5,24 +5,31 @@ import jwtDecode from 'jwt-decode';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
     const token = request.headers.get('Authorization');
-    if( token != undefined) {
-      const updatedToken = token.replace("Bearer ", "").trim()
-      const decryptedValue = jwtDecode(updatedToken);
-      const clientIdSecret = decryptedValue['sub']?.split("_")[2];
-      if(clientIdSecret != undefined && process.env.NEXTAUTH_SECRET === clientIdSecret) {
-        console.log("GOT ACCESS")
+    if(pathname.startsWith('/api')) { // This is the API Authorization
+      if( token != undefined) {
+        const updatedToken = token.replace("Bearer ", "").trim()
+        const decryptedValue = jwtDecode(updatedToken);
+        const clientIdSecret = decryptedValue['sub']?.split("_")[2];
+        if(clientIdSecret != undefined && process.env.NEXTAUTH_SECRET === clientIdSecret) {
+          return NextResponse.next();
+        }else {
+          return NextResponse.redirect(new URL('/login', request.url))
+        }
       }else {
-        console.log("ROUter to login...")
+        return NextResponse.redirect(new URL('/login', request.url))
       }
+    }else {
+      //Page level auth
+      //TODO: Check the role logic here before forwarding
+      return NextResponse.next();
     }
-    
-  // return NextResponse.redirect(new URL('/about-2', request.url))
 
-  return NextResponse.next();
+    
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/account/users', '/api/:path*']
+  matcher: ['/account/:path*', '/api/account/:path*', '/api/calendar/:path*', '/api/notes/:path*', '/api/timesheet/:path*', '/accounts']
 }
