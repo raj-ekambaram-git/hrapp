d830987f8233200dc968fbe6cbf6e99eddbc4826
Raj Ekambaram <75325809+raj-ekambaram-git@users.noreply.github.com>
2023-01-11 01:33:08 -0600

    Comit.

--- components/styles/accordionTheme.jsx
+++ components/styles/accordionTheme.jsx
@@ -5,31 +5,37 @@ const { definePartsStyle, defineMultiStyleConfig } =
   createMultiStyleConfigHelpers(accordionAnatomy.keys)
 
 const mainPage = definePartsStyle({
+    root: {
+      borderRadius: 'lg'
+    },
     container: {
         marginBottom: "1rem",
-        width: "60%"
+        width: "60%",
+        borderRadius: 'lg'
       },
     button: {
-        
-         bgColor: "table_tile"
+         bgColor: "table_tile",
+         borderRadius: 'lg'
     },
-  panel: {
-    border: '1px',
-  },
-  icon: {
-    border: '1px solid',
-    borderColor: 'gray.200',
-    background: 'gray.200',
-    borderRadius: 'full',
-    color: 'gray.500',
+    
+    panel: {
+      border: '1px',
+      borderRadius: 'lg'
+    },
+    icon: {
+      border: '1px solid',
+      borderColor: 'gray.200',
+      background: 'gray.200',
+      borderRadius: 'full',
+      color: 'gray.500',
 
-    _dark: {
-      borderColor: 'gray.600',
-      background: 'gray.600',
-      color: 'gray.400',
+      _dark: {
+        borderColor: 'gray.600',
+        background: 'gray.600',
+        color: 'gray.400',
+      },
     },
-  },
-})
+  })
 
 
 const baseStyle = definePartsStyle({
