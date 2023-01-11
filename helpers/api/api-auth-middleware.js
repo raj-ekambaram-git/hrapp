77f93e259f1132b979d4f45e1e07a1e4bd87c4a0
Raj Ekambaram <75325809+raj-ekambaram-git@users.noreply.github.com>
2023-01-10 23:16:02 -0600

    Added more security

---
+++ helpers/api/api-auth-middleware.js
@@ -0,0 +1,31 @@
+import jwtDecode from 'jwt-decode';
+
+export { isAuthorized };
+
+function isAuthorized(req) {
+    // make authenticate path public
+    if (req.url === '/api/authenticate') {
+        return;
+    }
+
+    // check for basic auth header
+    if (!req.headers.authorization || req.headers.authorization.indexOf('Bearer ') === -1) {
+        return false;
+    }
+
+    // verify auth credentials
+    const token = req.headers.authorization;
+    if( token != undefined) {
+        const updatedToken = token.replace("Bearer ", "").trim()
+        const decryptedValue = jwtDecode(updatedToken);
+        const clientIdSecret = decryptedValue['sub']?.split("_")[2];
+        if(clientIdSecret != undefined && process.env.NEXTAUTH_SECRET === clientIdSecret) {
+          return true;
+        }else {
+            return false;
+        }
+    }else {
+        return false;
+    }
+
+}
--- pages/api/account/[accountId]/detail.ts
+++ pages/api/account/[accountId]/detail.ts
@@ -2,25 +2,34 @@
 
 import { NextApiRequest, NextApiResponse } from "next"
 import prisma from "../../../../lib/prisma";
+import { isAuthorized } from '../../../../helpers/api/api-auth-middleware';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== 'GET') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
+  
+  if(isAuthorized(req)) {
+    try {
+      const accountId = req.query?.accountId;
+      const account = await prisma.account.findUnique({
+        where: {
+          id: parseInt(accountId.toString()),
+        },
+        include: {
+          address: true,
+        }
+      })
+      console.log("account:::"+JSON.stringify(account))
+        res.status(200).json(account);
+    } catch (error) {
+      console.log(error)
+      res.status(400).json({ message: 'Something went wrong while updating' })
+    }
 
-  try {
-    const accountId = req.query?.accountId;
-    const account = await prisma.account.findUnique({
-      where: {
-        id: parseInt(accountId.toString()),
-      },
-      include: {
-        address: true,
-      }
-    })
-      res.status(200).json(account);
-  } catch (error) {
-    console.log(error)
-    res.status(400).json({ message: 'Something went wrong while updating' })
+  }else {
+    res.status(400).json({ status: 400, message: 'Invalid Authentication Credentials' });
   }
+
+
 }
