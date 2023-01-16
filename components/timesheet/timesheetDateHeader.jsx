2b098b9953f579ff04e802847d06fac1b8672a3e
Raj Ekambaram <75325809+raj-ekambaram-git@users.noreply.github.com>
2023-01-16 13:37:29 -0600

    Commit.

--- components/timesheet/timesheetDateHeader.jsx
+++ components/timesheet/timesheetDateHeader.jsx
@@ -74,7 +74,6 @@ const TimesheetDateHeader = (props) => {
                                 </>
                             ) : (
                                 <>
-                                fsgsg
                                 </>
                             )}
                                    
