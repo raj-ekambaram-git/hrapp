46f67585c084a455d73718295bb15ad85160de8b
Raj Ekambaram <75325809+raj-ekambaram-git@users.noreply.github.com>
2023-01-19 10:39:38 -0600

    Commit.

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -5,9 +5,9 @@ const {join} = require('path');
  */
 module.exports = {
   // Changes the cache location for Puppeteer.
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
+  cacheDirectory: join(__dirname, '/app/.cache', 'puppeteer'),
 };
 
-const browser = await puppeteer.launch({
-  ignoreDefaultArgs: ['--disable-extensions','--no-sandbox', '--disable-setuid-sandbox','--aggressive-cache-discard'],
-});
+// const browser = await puppeteer.launch({
+//   ignoreDefaultArgs: ['--disable-extensions','--no-sandbox', '--disable-setuid-sandbox','--aggressive-cache-discard'],
+// });
