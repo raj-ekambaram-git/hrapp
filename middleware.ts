d40a6f5d0a69dc5dd13bb33bc5c4cc5e3c602638
Raj Ekambaram <75325809+raj-ekambaram-git@users.noreply.github.com>
2023-01-18 20:39:23 -0600

    Committed.

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import jwtDecode from 'jwt-decode';
+import cache from "memory-cache";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
@@ -14,6 +15,7 @@ export function middleware(request: NextRequest) {
         const decryptedValue = jwtDecode(updatedToken);
         const clientIdSecret = decryptedValue['sub']?.split(":")[2];
         console.log("User Roles::"+decryptedValue['sub']?.split(":")[3])
+        // hasAccess(pathname,updatedToken)
         if(clientIdSecret != undefined && process.env.NEXTAUTH_SECRET === clientIdSecret) {
           return NextResponse.next();
         }else {
@@ -29,6 +31,33 @@ export function middleware(request: NextRequest) {
     }
 
     
+}
+
+async function hasAccess(url, token){
+
+  const cachedResponse = cache.get(url);
+  if (cachedResponse) {
+    console.log("cachedResponse:::"+JSON.stringify(cachedResponse))
+    return cachedResponse;
+  } else {
+    const hours = 24;
+    const response = await fetch(process.env.BASE_API_URL+`/access/roles/`);
+    const rolesData = await response.json();
+    cache.put(url, rolesData, hours * 1000 * 60 * 60);
+    console.log("Daata Middleware ::"+JSON.stringify(rolesData))
+      const decryptedValue = jwtDecode(token);
+      const userRoles = decryptedValue['sub']?.split(":")[3];
+      const userRolesArray = userRoles.split(",");
+      const urlAllowed = userRolesArray?.map((role, index) => {
+          if(rolesData[role].allowedPages?.includes(url)){
+              return true;
+          }else{
+              return false;
+          }
+      });
+      return urlAllowed.includes(true);
+
+  }
 }
 
 // See "Matching Paths" below to learn more
