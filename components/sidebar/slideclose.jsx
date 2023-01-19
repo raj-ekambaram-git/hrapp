ffbd82e9d4dc393bcad342cec1f8e9f0baba109e
Raj Ekambaram <75325809+raj-ekambaram-git@users.noreply.github.com>
2023-01-18 20:12:05 -0600

    Commit.

--- components/sidebar/slideclose.jsx
+++ components/sidebar/slideclose.jsx
@@ -21,70 +21,70 @@ const Slideclose = (props) => {
   return (
     <div className={styles.main}>
         <div className={styles.iconsname}>
-        {props.allowedModule.includes("account")?(<>
+        {props.allowedModule?.includes("account")?(<>
           <Link href={`/accounts`} styles={({isActive}) => (isActive ? navbaractive: navbarnotactive)}>  
             <Box padding={2}>
               Accounts
             </Box>
           </Link>
         </>):""}
-        {props.allowedModule.includes("user")?(<>
+        {props.allowedModule?.includes("user")?(<>
           <Link href={`/account/users`} styles={({isActive}) => (isActive ? navbaractive: navbarnotactive)}>  
             <Box padding="8px">
               Users
             </Box>
           </Link>
         </>):""}
-        {props.allowedModule.includes("vendor")?(<>
+        {props.allowedModule?.includes("vendor")?(<>
           <Link href={`/account/vendors`} styles={({isActive}) => (isActive ? navbaractive: navbarnotactive)}>  
             <Box padding="8px">
               Vendors
             </Box>
           </Link>
         </>):""}
-        {props.allowedModule.includes("project")?(<>
+        {props.allowedModule?.includes("project")?(<>
           <Link href={`/account/projects`} styles={({isActive}) => (isActive ? navbaractive: navbarnotactive)}>  
             <Box padding="8px">
               Projects
             </Box>
           </Link>         
         </>):""}
-        {props.allowedModule.includes("invoice")?(<>
+        {props.allowedModule?.includes("invoice")?(<>
           <Link href={`/account/invoices`} styles={({isActive}) => (isActive ? navbaractive: navbarnotactive)}>  
             <Box padding="8px">
               Invoices
             </Box>
           </Link>
         </>):""}
-        {props.allowedModule.includes("timesheet")?(<>
+        {props.allowedModule?.includes("timesheet")?(<>
           <Link href={`/account/user/timesheets`} styles={({isActive}) => (isActive ? navbaractive: navbarnotactive)}>  
             <Box padding="8px">
               Timesheets
             </Box>
           </Link>    
         </>):""}
-        {props.allowedModule.includes("expense")?(<>
+        {props.allowedModule?.includes("expense")?(<>
           <Link href={`/account/user/expenses`} styles={({isActive}) => (isActive ? navbaractive: navbarnotactive)}>  
             <Box padding="8px">
                Expenses
             </Box>
           </Link>              
         </>):""}
-        {props.allowedModule.includes("timesheet_approval")?(<>
+        {props.allowedModule?.includes("timesheet_approval")?(<>
           <Link href={`/account/user/timesheets/approval`} styles={({isActive}) => (isActive ? navbaractive: navbarnotactive)}>  
             <Box padding="8px">
               Timesheet Approvals
             </Box>
           </Link>    
         </>):""}
-        {props.allowedModule.includes("expense_approval")?(<>
+        {props.allowedModule?.includes("expense_approval")?(<>
           <Link href={`/account/user/expenses/approval`} styles={({isActive}) => (isActive ? navbaractive: navbarnotactive)}>  
             <Box padding="8px">
               Expense Approvals
             </Box>
           </Link>           
         </>):""}
-        {props.allowedModule.includes("setting")?(<>
+        {props.allowedModule?.includes("setting")?(<>
           <Link href={`/account/user/expenses/approval`} styles={({isActive}) => (isActive ? navbaractive: navbarnotactive)}>  
               <Box padding="8px">
                 Settings
