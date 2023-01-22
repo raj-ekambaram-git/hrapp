84457810684dee57f0ada1cca45a983333dbeb1d
Raj Ekambaram <75325809+raj-ekambaram-git@users.noreply.github.com>
2023-01-21 22:50:00 -0600

    Commit

--- components/reports/projectDashboard.jsx
+++ components/reports/projectDashboard.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from "react";
 import { projectService, userService } from "../../services";
-
+import Chart from 'chart.js/auto'
+import { Box } from "@chakra-ui/react";
+import { util } from "../../helpers/util";
 
 
 export default function ProjectDashboard(props) {
 
   const projectId = parseInt("2");
+
 
   useEffect(() => {
     console.log("ProjectDashboard.selectedReport::")
@@ -16,9 +19,52 @@ export default function ProjectDashboard(props) {
     const responseData = await projectService.getProjectBudgetDetails(projectId, userService.getAccountDetails().accountId);
 
     console.log("responseData:::"+JSON.stringify(responseData))
+
+    let usedBudget = 0;
+    responseData.invoice?.map(inv => usedBudget = parseFloat(usedBudget)+parseFloat(inv.total))
+    console.log("usedBudget:::"+usedBudget)
+
+
+    const data = [
+      { key: "Used $"+usedBudget, value: usedBudget },
+      { key: "Remaining $"+(util.getZeroPriceForNull(responseData.budget)-usedBudget), value: (util.getZeroPriceForNull(responseData.budget)-usedBudget) },
+    ];
+
+    const chart = new Chart(
+      document.getElementById('projectBudget'),
+      {
+        type: 'doughnut',
+        data: {
+          labels: data.map(row => row.key),
+          datasets: [
+            {
+              data: data.map(row => row.value)
+            }
+          ]
+        },
+        options: {
+          responsive: true,
+          plugins: {
+            legend: {
+              position: 'top',
+            },
+            title: {
+              display: true,
+              text: 'Project Budget: $'+util.getZeroPriceForNull(responseData.budget)
+            }
+          }
+        }
+      }
+    );
+
   }
 
   return (
-    <>Project</>
+    <>    
+      <Box maxW="25%">
+          <canvas id="projectBudget"></canvas>
+          <canvas id="projectBudget"></canvas>
+      </Box>
+    </>
   );
 }
