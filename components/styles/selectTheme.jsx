403956e9e0500a01aca5cc2bd9656c36f97c55a5
Raj Ekambaram <75325809+raj-ekambaram-git@users.noreply.github.com>
2023-01-25 16:27:12 -0600

    Commit.

--- components/styles/selectTheme.jsx
+++ components/styles/selectTheme.jsx
@@ -1,7 +1,43 @@
-import { defineStyle, defineStyleConfig } from '@chakra-ui/react'
+import { selectAnatomy } from '@chakra-ui/anatomy'
+import { createMultiStyleConfigHelpers, defineStyle } from '@chakra-ui/react'
 
-export const selectTheme = defineStyleConfig({
+const { defineMultiStyleConfig, definePartsStyle } = createMultiStyleConfigHelpers(
+  selectAnatomy.keys,
+)
+
+const baseStyle = definePartsStyle({
+  // define the part you're going to style
+  field: {
+    color: 'teal.800', // change the input text color
+    border: '1px solid',
+    borderColor: 'gray.300',
+    background: 'gray.50',
+    borderRadius: 'full',
+
+    // Let's also provide dark mode alternatives
+    _dark: {
+      borderColor: 'gray.600',
+      background: 'gray.800',
+    },    
+    _readOnly: {
+      boxShadow: "none !important",
+      userSelect: "all",
+    },
+    _invalid: {
+      borderColor: "red",
+      boxShadow: `0px 1px 0px 0px red`,
+    },
+    _focusVisible: {
+      borderColor: "teal.700",
+    },
+  },
+  
+})
+
+export const selectTheme = defineMultiStyleConfig({
   defaultProps: {
     size: 'sm',
-  },
+    variant: baseStyle,
+  },baseStyle,
+  variants: {}
 })
