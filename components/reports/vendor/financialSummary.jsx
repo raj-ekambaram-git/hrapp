5ef0b6cde40b1ec3f0a061aad68a45553e56d750
Raj Ekambaram <75325809+raj-ekambaram-git@users.noreply.github.com>
2023-01-24 20:59:28 -0600

    Commit.

--- components/reports/vendor/financialSummary.jsx
+++ components/reports/vendor/financialSummary.jsx
@@ -1,4 +1,4 @@
-import { Box, Card, CardBody, CardHeader, Divider, Heading, HStack, Stack } from "@chakra-ui/react";
+import { Box, Card, CardBody, CardHeader, Tooltip, Heading, HStack, Stack } from "@chakra-ui/react";
 import { ExpenseStatus, InvoiceStatus } from "@prisma/client";
 import React, { useEffect, useState } from "react";
 import { util } from "../../../helpers/util";
@@ -11,12 +11,15 @@ export default function FinancialSummary(props) {
   const [invoiceNotPaid, setInvoiceNotPaid] = useState(0);
   const [notInvoiced, setNotInvoiced] = useState(0);
   const [projectCost, setProjectCost] = useState(0);
+  const [submittedProjectCost, setSubmittedProjectCost] = useState(0);
+  const [estimatedProjectCost, setEstimatedProjectCost] = useState(0);
+  const [paidProjectCost, setPaidProjectCost] = useState(0);
   const [billableExpense, setBillableExpense] = useState(0);
   const [nonbillableExpense, setNonbillableExpense] = useState(0);
   const [paidExpense, setPaidExpense] = useState(0);
   const [unpaidExpense, setUnpaidExpense] = useState(0);
   const [netRevenue, setNetRevenue] = useState(0);
-  
+  const [estimatedNetRevenue, setEstimatedNetRevenue] = useState(0);
 
   useEffect(() => {
     if(props.projects && props.projects.length > 0) {
@@ -37,14 +40,14 @@ export default function FinancialSummary(props) {
     let invoicePaid = 0;
 
     //EXPENSE
-    let expProjectCost = 0
+    let expSubmiitedProjectCost = 0
     let expBillable = 0
     let expNonBillable = 0
     let expenseTotal = 0;
     let expensePaid = 0;
     let expenseNotInvoiced = 0;
-
-        
+    let expPaidProjectCost = 0;
+    let estProjectCost = 0;
     props.projects?.map(project => {      
       totalBudget = totalBudget+util.getZeroPriceForNull(project.budget)
       totalUsedBudget = totalUsedBudget+util.getZeroPriceForNull(project.usedBudget)
@@ -67,30 +70,42 @@ export default function FinancialSummary(props) {
           exp.status === ExpenseStatus.Approved || exp.status === ExpenseStatus.Invoiced ){
             const expenseAmounts = util.getTotalBillableExpense(exp.expenseEntries);
             expensePaid = parseFloat(expensePaid)+parseFloat(exp?.paidAmount)
-            expProjectCost = expProjectCost+expenseAmounts?.totalProjectCost; 
+            expSubmiitedProjectCost = expSubmiitedProjectCost+expenseAmounts?.totalProjectCost; 
             expBillable = expBillable+expenseAmounts?.billableExpense;
             expNonBillable = expNonBillable+expenseAmounts?.nonBillableExpense;
             if(exp.status === ExpenseStatus.PartiallyPaid || exp.status === ExpenseStatus.Approved) {
               expenseNotInvoiced = expenseNotInvoiced+expBillable;
             }
+            if(exp.status === ExpenseStatus.PartiallyPaid || exp.status === ExpenseStatus.Paid) {
+              expPaidProjectCost = expPaidProjectCost+expenseAmounts?.totalProjectCost;
+            }
           }
         
       })
+      
+      project?.projectResource?.map(resource => {
+        estProjectCost = estProjectCost+((parseFloat(resource.usedBudget)/parseFloat(resource.unitPrice))*parseFloat(resource.cost))
+      })      
 
     })
+
+
+
 
     setTotalRevenue(util.getZeroPriceForNull(totalUsedBudget)+util.getZeroPriceForNull(totalMiscUsedBudget))
 
     setInvoiced(invoicedTotal)  
     setInvoicePaid(invoicePaid)
     setInvoiceNotPaid(invoicedTotal-invoicePaid)
-
+    setPaidProjectCost(expPaidProjectCost)
+    setEstimatedProjectCost(estProjectCost)
     setBillableExpense(expBillable)
     setNonbillableExpense(expNonBillable)
-    setProjectCost(expProjectCost)
+    setSubmittedProjectCost(expSubmiitedProjectCost)
     setPaidExpense(expensePaid)
     setUnpaidExpense(parseFloat(expenseTotal)-parseFloat(expensePaid))
-    setNetRevenue((util.getZeroPriceForNull(totalUsedBudget)+util.getZeroPriceForNull(totalMiscUsedBudget)-(util.getZeroPriceForNull(expProjectCost)+util.getZeroPriceForNull(expBillable)+util.getZeroPriceForNull(expNonBillable))))    
+    setNetRevenue((util.getZeroPriceForNull(totalUsedBudget)+util.getZeroPriceForNull(totalMiscUsedBudget)-(util.getZeroPriceForNull(expSubmiitedProjectCost)+util.getZeroPriceForNull(expBillable)+util.getZeroPriceForNull(expNonBillable))))    
+    setEstimatedNetRevenue((util.getZeroPriceForNull(totalUsedBudget)+util.getZeroPriceForNull(totalMiscUsedBudget)-(util.getZeroPriceForNull(estProjectCost)+util.getZeroPriceForNull(expBillable)+util.getZeroPriceForNull(expNonBillable))))    
   }
 
 
@@ -151,7 +166,7 @@ export default function FinancialSummary(props) {
                   </Box>              
                 </HStack>                                                
               </Stack>
-              <Stack width="50%">
+              <Stack width="70%">
                 <HStack>
                   <Box textAlign="left" fontWeight="semibold" fontStyle="italic">
                     Expense
@@ -159,10 +174,32 @@ export default function FinancialSummary(props) {
                 </HStack>
                 <HStack>
                   <Box width="60%" textAlign="right">
-                    Project Cost :
+                    <Tooltip label="Includes all the resource cost for the project.">
+                      Estimated Project Cost :
+                    </Tooltip>
                   </Box>
                   <Box width="50%" textAlign="left" fontWeight="semibold">
-                    {util.getWithCurrency(projectCost)}
+                    {util.getWithCurrency(estimatedProjectCost)}
+                  </Box>              
+                </HStack>
+                <HStack>
+                  <Box width="60%" textAlign="right">
+                  <Tooltip label="Submitted/Approved project cost (resource cost).">
+                    Submiitted Project Cost :
+                  </Tooltip>
+                  </Box>
+                  <Box width="50%" textAlign="left" fontWeight="semibold">
+                    {util.getWithCurrency(submittedProjectCost)}
+                  </Box>              
+                </HStack>
+                <HStack>
+                  <Box width="60%" textAlign="right">
+                    <Tooltip label="Project coast paid as of today.">
+                      Paid Project Cost :
+                    </Tooltip>
+                  </Box>
+                  <Box width="50%" textAlign="left" fontWeight="semibold">
+                    {util.getWithCurrency(paidProjectCost)}
                   </Box>              
                 </HStack>
                 <HStack>
@@ -209,12 +246,24 @@ export default function FinancialSummary(props) {
             </HStack>
             <HStack>
                   <Box width="60%" textAlign="right" fontWeight="semibold" fontStyle="italic">
-                    Net Revenue :
+                    <Tooltip label="Estimated net revenue after removing all the known costs as of now.">
+                      Estimated Net Revenue :
+                    </Tooltip>
+                  </Box>
+                  <Box width="50%" textAlign="left" fontWeight="semibold" color={util.getZeroPriceForNull(estimatedNetRevenue) > 0 ? 'debit_amount':"credit_amount"}>
+                    {util.getWithCurrency(estimatedNetRevenue)}
+                  </Box>  
+            </HStack>
+            <HStack>
+                  <Box width="60%" textAlign="right" fontWeight="semibold" fontStyle="italic">
+                    <Tooltip label="Net revenue as of today excluding all the project expenses (aproved resource cost, billabld and non-billable expense) approved">
+                      Actual Net Revenue :
+                    </Tooltip>
                   </Box>
                   <Box width="50%" textAlign="left" fontWeight="semibold" color={util.getZeroPriceForNull(netRevenue) > 0 ? 'debit_amount':"credit_amount"}>
                     {util.getWithCurrency(netRevenue)}
                   </Box>  
-            </HStack>
+            </HStack>                
           </Stack>
         </CardBody>
       </Card>
