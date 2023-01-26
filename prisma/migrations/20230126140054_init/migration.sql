d1fa4787a02015d594ff6f0c3235c1da24468a05
Raj Ekambaram <75325809+raj-ekambaram-git@users.noreply.github.com>
2023-01-26 08:01:28 -0600

    Commit.

---
+++ prisma/migrations/20230126140054_init/migration.sql
@@ -0,0 +1,38 @@
+-- AlterEnum
+ALTER TYPE "AccountStatus" ADD VALUE 'MarkForDelete';
+
+-- AlterEnum
+ALTER TYPE "DocumentStatus" ADD VALUE 'MarkForDelete';
+
+-- AlterEnum
+ALTER TYPE "ExpenseEntryStatus" ADD VALUE 'MarkForDelete';
+
+-- AlterEnum
+ALTER TYPE "ExpenseStatus" ADD VALUE 'MarkForDelete';
+
+-- AlterEnum
+ALTER TYPE "ExpenseTransactionStatus" ADD VALUE 'MarkForDelete';
+
+-- AlterEnum
+ALTER TYPE "InvoiceStatus" ADD VALUE 'MarkForDelete';
+
+-- AlterEnum
+ALTER TYPE "InvoiceTransactionStatus" ADD VALUE 'MarkForDelete';
+
+-- AlterEnum
+ALTER TYPE "ProjectStatus" ADD VALUE 'MarkForDelete';
+
+-- AlterEnum
+ALTER TYPE "TimesheetStatus" ADD VALUE 'MarkForDelete';
+
+-- AlterEnum
+ALTER TYPE "UnitOfMeasure" ADD VALUE 'Expense';
+
+-- AlterEnum
+ALTER TYPE "UserStatus" ADD VALUE 'MarkForDelete';
+
+-- AlterEnum
+ALTER TYPE "VendorStatus" ADD VALUE 'MarkForDelete';
+
+-- AlterEnum
+ALTER TYPE "VendorUserStatus" ADD VALUE 'MarkForDelete';
