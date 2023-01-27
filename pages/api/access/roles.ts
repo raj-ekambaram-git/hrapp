65a679204107e152c23ec8e86409734a84a007d0
Raj Ekambaram <75325809+raj-ekambaram-git@users.noreply.github.com>
2023-01-27 16:46:23 -0600

    Commiit.

--- pages/api/access/roles.ts
+++ pages/api/access/roles.ts
@@ -1,11 +1,24 @@
-import path from 'path';
-import { promises as fs } from 'fs';
+import S3 from "aws-sdk/clients/s3";
 
 export default async function handler(req, res) {
   //Find the absolute path of the json directory
-  const jsonDirectory = path.join(process.cwd(), 'data');
+  // const jsonDirectory = path.join(process.cwd(), 'data');
   //Read the json data file data.json
-  const fileContents = await fs.readFile(jsonDirectory + '/roles.json', 'utf8');
+  // const fileContents = await fs.readFile(jsonDirectory + '/roles.json', 'utf8');
   //Return the content of the data file in json format
-  res.status(200).json(JSON.parse(fileContents));
+
+  const s3 = new S3({
+    region: process.env.ACCESS_REGION,
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_KEY,
+  });
+
+  const fileParams = {
+    Bucket: process.env.BUCKET_NAME,
+    Key: 'configurations/roles.json',
+  }
+
+  const data = (await (s3.getObject(fileParams).promise())).Body.toString('utf-8')
+  console.log("DAATTAAA:"+data)
+  res.status(200).json(JSON.parse(data));
 }
