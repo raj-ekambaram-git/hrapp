994ae96da9179f478d43c637e8e1b575ce35a4b8
Raj Ekambaram <75325809+raj-ekambaram-git@users.noreply.github.com>
2023-02-08 20:30:44 -0600

    Commit.

--- pages/api/account/[accountId]/workflow/step/[stepId]/index.ts
+++ pages/api/account/[accountId]/workflow/step/[stepId]/index.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import { InvoiceStatus, ProjectStatus, UserStatus, VendorStatus, WorkFlowStepStatus, WorkFlowType } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next"
 import prisma from "../../../../../../../lib/prisma";
 
@@ -18,8 +19,66 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         id: stepRequest.id,
       },
       data: stepRequest,
-      
+      include: {
+        workFlow: {
+          select: {
+            type: true,
+            typeId: true,
+            workFlowSteps: {
+              where: {
+                status: {
+                  notIn: [WorkFlowStepStatus.Complete]
+                }
+              }
+            }
+          }
+        }
+      }      
     });
+
+    console.log("savedStep:::"+JSON.stringify(savedStep))
+    //Check if there are any more non completed items, if all are completed, go back and mark the type ID as Approved
+    if(savedStep && savedStep.workFlow && savedStep.workFlow?.workFlowSteps && savedStep.workFlow?.workFlowSteps.length == 0) {
+
+      if(savedStep.workFlow?.type === WorkFlowType.Vendor) {
+        const savedVendor = await prisma.vendor.update({
+          where: {
+            id: parseInt(savedStep.workFlow?.typeId?.toString()),
+          },
+          data: {
+            status: VendorStatus.Active
+          }
+        });
+      } else if (savedStep.workFlow?.type === WorkFlowType.Project) {
+        const savedProject = await prisma.project.update({
+          where: {
+            id: parseInt(savedStep.workFlow?.typeId?.toString()),
+          },
+          data: {
+            status: ProjectStatus.Open
+          }
+        });        
+
+      } else if (savedStep.workFlow?.type === WorkFlowType.User) {
+        const savedUser = await prisma.user.update({
+          where: {
+            id: parseInt(savedStep.workFlow?.typeId?.toString()),
+          },
+          data: {
+            status: UserStatus.Approved
+          }
+        });
+      } else if (savedStep.workFlow?.type === WorkFlowType.Invoice) {
+        const savedInvoice = await prisma.invoice.update({
+          where: {
+            id: parseInt(savedStep.workFlow?.typeId?.toString()),
+          },
+          data: {
+            status: InvoiceStatus.Submitted
+          }
+        });
+      }
+    }
     res.status(200).json(savedStep);
   } catch (error) {
     console.log(error)
