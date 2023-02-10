76b55c962432490a5ce381cae681716a4aff326f
Raj Ekambaram <75325809+raj-ekambaram-git@users.noreply.github.com>
2023-02-09 18:06:06 -0600

    Commit.

--- components/styles/badgeTheme.jsx
+++ components/styles/badgeTheme.jsx
@@ -1,8 +1,16 @@
 import { defineStyle, defineStyleConfig } from '@chakra-ui/react'
 
+
+const status = defineStyle({
+  border: "1px",
+  backgroundColor: "gray.50",
+  fontWeight: "900"
+})
+
 export const badgeTheme = defineStyleConfig({
   defaultProps: {
     size: 'sm',
-    variant: "outline"
+    variant: "status",
   },
+  variants: { status}
 })
