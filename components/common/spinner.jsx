0818e4801d4c5cb21e4c33d634356815e1ebd42f
Raj Ekambaram <75325809+raj-ekambaram-git@users.noreply.github.com>
2023-02-09 19:15:28 -0600

    Commit.

--- components/common/spinner.jsx
+++ components/common/spinner.jsx
@@ -1,9 +1,12 @@
+import styles from "./styles/spinner.module.css"
+
 export { Spinner };
 
 function Spinner() {
     return (
-        <div className="text-center p-3">
-            <span className="spinner-border spinner-border-lg align-center"></span>
+        <div class={styles.loading}>
+            <div class={styles.loader}></div>
         </div>
+        
     );
 }
